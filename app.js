6542cdb373ba95c044750752d7bd9080f1cba0e0
Marco Bullin <marco.bullin@sueddeutsche.de>
2018-09-01 16:26:15 +0200

    Add project dependencies and build a connection to a mongo database

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 'use strict';
 
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb://mongodb:27017/techChallenge', { useNewUrlParser: true });
+
+app.use(bodyParser.json());
 
 routes(app);
 
