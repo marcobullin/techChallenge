d99183442235079259e9e3eeec22d60cbf79b06e
Marco Bullin <marco.bullin@sueddeutsche.de>
2018-09-02 11:56:01 +0200

    Refactoring - simplify request handling and move validation to actual car advert module

--- carAdvert/carAdvert.js
+++ carAdvert/carAdvert.js
@@ -1,41 +1,47 @@
 const CarAdvert = require('./model/carAdvert');
+const carAdvertValidator = require('./carAdvertValidator');
 
 module.exports = {
     getAll: sortField => {
-        const sort = sortField ? { [sortField] : 1 } : { _id: 1 };
-
-        return CarAdvert.find({}).sort(sort);
+        return carAdvertValidator.validateSortParam(sortField)
+            .then(() => {
+                const sort = sortField ? { [sortField] : 1 } : { _id: 1 };
+                return CarAdvert.find({}).sort(sort);
+            });
     },
     
     create: ({ title, fuel, price, isNew, mileage, firstRegistration }) => {
-        const carAdvert = new CarAdvert({ 
-            title,
-            fuel,
-            price,
-            new: isNew,
-            mileage,
-            firstRegistration: firstRegistration ? new Date(firstRegistration): null
-        })
+        return carAdvertValidator.validate({ title, fuel, price, isNew, mileage, firstRegistration })
+            .then(() => {
+                const carAdvert = new CarAdvert({
+                    title,
+                    fuel,
+                    price,
+                    new: isNew,
+                    mileage,
+                    firstRegistration: firstRegistration ? new Date(firstRegistration): null
+                })
 
-        return carAdvert.save();
+                return carAdvert.save();
+            });
     },
 
-    get: (id) => {
-        return CarAdvert.findOne({ _id: id });
+    get: id => {
+        return carAdvertValidator.validateId(id)
+            .then(() => CarAdvert.findOne({ _id: id }));
     },
         
     update: (id, { title, fuel, price, isNew, mileage, firstRegistration }) => {
-        return CarAdvert.updateOne({ _id: id }, { 
-            title,
-            fuel,
-            price,
-            new: isNew,
-            mileage,
-            firstRegistration: firstRegistration ? new Date(firstRegistration): null
-        });
+        return carAdvertValidator.validateId(id)
+            .then(() => carAdvertValidator.validate({ title, fuel, price, isNew, mileage, firstRegistration }))
+            .then(() => CarAdvert.updateOne({ _id: id }, {
+                title, fuel, price, new: isNew, mileage,
+                firstRegistration: firstRegistration ? new Date(firstRegistration): null
+            }));
     },
 
     remove: id => {
-        return CarAdvert.deleteOne({ _id: id });
+        return carAdvertValidator.validateId(id)
+            .then(() => CarAdvert.deleteOne({ _id: id }));
     }
 }
