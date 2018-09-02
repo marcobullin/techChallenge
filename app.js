6c95883ad71b4782090ed934e0d62539d6b43448
Marco Bullin <marco.bullin@sueddeutsche.de>
2018-09-02 10:19:22 +0200

    Add CORS header to allow requests from other origins and handle preflight requests as well

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
 const routes = require('./routes');
 
 const app = express();
@@ -11,10 +10,15 @@ const app = express();
 mongoose.connect('mongodb://mongodb:27017/techChallenge', { useNewUrlParser: true });
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTION');
+    next();
+});
 
 routes(app);
 
 app.listen(process.env.PORT || 3000, (req, res) => {
     console.log(`Server is running on PORT "${process.env.PORT || 3000}"`);
 });
-
